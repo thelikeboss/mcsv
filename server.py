8de9ecb5119ae4fe6c3eb8d4cb19252bd8bd4ef0
thelikeboss <80713597+thelikeboss@users.noreply.github.com>
2024-08-21 04:45:06 +0000

    Mundo guardado 2024-08-21

--- server.py
+++ server.py
@@ -1,11 +1,4 @@
-# Versiones disponibles
-# forge, mohist, fabric, vanilla, paper
 
-# Puedes instalar mohist después de instalar forge desde el menú de gestionar
-# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
-
-# No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
